7669de4b120567be84d7503c39ed219cad563703
jeanpaullm <jeanpaullm@gmail.com>
2018-07-30 20:30:36 -0600

    added text inputs

--- app.js
+++ app.js
@@ -4,23 +4,27 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(express.static('public'));
+app.use("/public",express.static('./public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/',function(req,res) {
+  res.redirect('/public/ejemplo.html');
+});
+
+app.get('/public',function(req,res) {
   //res.writeHead(200,{"Content-Type":"application/json"});
   //res.sendFile(path.join(__dirname +  '/ejemplo.html'));
-  res.render("ejemplo");
+  res.render("public/ejemplo");
 });
 
 app.post('/',function(req,res) {
-  console.log("texto1 " + req.body.texto_1);
-  console.log("texto2 " + req.body.texto_2);
-  console.log("texto3 " + req.body.texto_3);
-  console.log("texto4 " + req.body.texto_4);
-  res.send("informacion publicada");
+  res.send("informacion publicada... \n" +
+           "texto 1: " + req.body.texto_1 + " " +
+           "texto 2: " + req.body.texto_2 + " " +
+           "texto 3: " + req.body.texto_3 + " " +
+           "texto 4: " + req.body.texto_4);
 });
 
 app.listen(8080);
