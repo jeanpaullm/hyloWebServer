46f6cf3a2b7ce36f61c62d26fe3c629c28c25c1f
jeanpaullm <jeanpaullm@gmail.com>
2018-08-02 18:26:34 -0600

    added graph

--- app.js
+++ app.js
@@ -13,18 +13,23 @@ app.get('/',function(req,res) {
   res.redirect('/public/ejemplo.html');
 });
 
-app.get('/public',function(req,res) {
+app.get('/public/ejemplo.html',function(req,res) {
   res.render("public/ejemplo");
 });
 
-app.post('/public',function(req,res) {
+app.post('/public/ejemplo.html',function(req,res) {
   var data = req.body;
   res.send("informacion publicada... \n" +
            "texto 1: " + data.texto1 + " " +
            "texto 2: " + data.texto2.slice(4,12) + " " +
            "texto 3: " + data.texto3.toUpperCase() + " " +
            "texto 4: " + data.texto4.charAt(3));
-  });
+});
+
+app.get('/public/graph.html',function(req,res) {
+  console.log("get graph regisrted");
+  res.render("public/graph");
+});
 
 const PORT = Number(process.env.PORT || 8080);
 app.listen(PORT);
